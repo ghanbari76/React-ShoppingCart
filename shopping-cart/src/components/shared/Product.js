6c2bac394e7c4328caca643ccfd52ddfaff81471
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-07-02 15:29:08 -0700

    Writing isInCart function to handle ADD_ITEM and INCREASE actions (product add to ShoppingCart)

--- shopping-cart/src/components/shared/Product.js
+++ shopping-cart/src/components/shared/Product.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 //Function
-import { shorten } from '../../helper/function';
+import { isInCart, shorten } from '../../helper/function';
+
+//Context
+import { CartContext } from '../../context/CartContextProvider';
 
 const Product = ({productData}) => {
+    const {state,dispatch} = useContext(CartContext)
     return (
         <div>
             <img src={productData.image} alt='product_image' style={{width:'200px'}} />
@@ -13,7 +17,10 @@ const Product = ({productData}) => {
             <div>
                 <Link to={`/products/${productData.id}`}>Detailes</Link>
                 <div>
-                    <button>Add to cart</button>
+                    {
+                        isInCart(state,productData.id) ? <button onClick={() => dispatch({type:"INCREASE",payload:productData})}>+</button> :
+                        <button onClick={() => dispatch({type:"ADD_ITEM",payload:productData})}>Add to cart</button>
+                    }
                 </div>
             </div>
         </div>
