7ee70ca17a2a1d318ba9755af626bd092e20cf37
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-07-01 18:01:09 -0700

    Define shortening function to shorten the name of each product

--- shopping-cart/src/components/shared/Product.js
+++ shopping-cart/src/components/shared/Product.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+//Function
+import { shorten } from '../../helper/function';
+
 const Product = ({productData}) => {
     return (
         <div>
             <img src={productData.image} alt='product_image' style={{width:'200px'}} />
-            <h3>{productData.title}</h3>
+            <h3>{shorten(productData.title)}</h3>
             <p>{productData.price}</p>
             <div>
                 <a href='#'>Detailes</a>
