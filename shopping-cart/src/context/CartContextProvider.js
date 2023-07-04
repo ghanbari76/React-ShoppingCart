cc50d4f70a000327ed3378f880728f72b3486aca
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-07-03 17:02:30 -0700

    Adding shopCart page and Cart component

--- shopping-cart/src/context/CartContextProvider.js
+++ shopping-cart/src/context/CartContextProvider.js
@@ -3,13 +3,13 @@ import React,{ useReducer,createContext } from 'react';
 const initialState = {
     selectedItems : [],
     itemsCounter : 0,
-    total : 0,
+    totalPrice : 0,
     checkout : false,
 }
 const sumItems = items => {
     const itemsCounter = items.reduce((totalCount,product) => totalCount + product.quantity,0);
-    const totalItemsPrice = items.reduce((totalPrice,product) => totalPrice + product.price*product.quantity,0).toFixed(2);
-    return {itemsCounter,totalItemsPrice}
+    const totalPrice = items.reduce((totalPrice,product) => totalPrice + product.price*product.quantity,0).toFixed(2);
+    return {itemsCounter,totalPrice};
 }
 
 const cartReducer = (state,action) => {
