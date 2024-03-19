fcf5148fb654f85fcb6a8983b7cf4ad3f7f960f0
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-03-19 15:12:50 -0700

    Adding search input to Navbar

--- shopping-cart/src/components/shared/Navbar.js
+++ shopping-cart/src/components/shared/Navbar.js
@@ -1,4 +1,4 @@
-import React,{ useContext } from 'react';
+import React,{ useContext, useState } from 'react';
 import { Link } from "react-router-dom"
 //Context
 import { CartContext } from '../../context/CartContextProvider';
@@ -9,10 +9,18 @@ import styles from "./Navbar.module.css"
 
 const Navbar = () => {
     const {state} = useContext(CartContext);
+    const [search,setSearch] = useState("");
+    const searchHandler = () => {
+        console.log(search)
+    }
     return (
         <div className={styles.mainContainer}>
             <div className={styles.container}>
                 <Link className={styles.productLink} to="/products">Products</Link>
+                <div className={styles.searchContainer}>
+                    <input type='text' placeholder="I'm shopping for..." value={search} onChange={event => setSearch(event.target.value.toLowerCase().trim())} />
+                    <button onClick={searchHandler}>Search</button>
+                </div>
                 <div className={styles.navbarRight}>
                     <div className={styles.loginContainer}>
                         <Link className={styles.loginLink} to="/login">Login</Link>
