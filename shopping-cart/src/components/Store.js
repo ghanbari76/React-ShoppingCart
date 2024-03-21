c2f86f8478875125849b6303d5fb3fc08ec7a644
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-03-20 17:56:49 -0700

    Adding Sidebar to Store

--- shopping-cart/src/components/Store.js
+++ shopping-cart/src/components/Store.js
@@ -14,6 +14,13 @@ const Store = () => {
     const [search,setSearch] = useState("");
     const searchHandler = () => {
         console.log(search)
+    };
+    const categoryHandler = (event) => {
+        const { tagName } = event.target;
+        const category = event.target.innerText.toLowerCase();
+
+        if (tagName !== "LI") return;
+        console.log(category);
     }
     return (
         <>
@@ -24,6 +31,19 @@ const Store = () => {
         <div className={styles.container} >
             <div className={styles.products}>
                 {products.map(product => <Product key={product.id} productData={product} /> )}
+            </div>
+            <div>
+                <div>
+                    FaListUl
+                    <p>Categories</p>
+                </div>
+                <ul onClick={categoryHandler}>
+                    <li>All</li>
+                    <li>Electronics</li>
+                    <li>Jewelery</li>
+                    <li>Men's Clothing</li>
+                    <li>Women's Clothing</li>
+                </ul>
             </div>
         </div>
         </>
